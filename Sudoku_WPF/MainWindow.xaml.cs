f8b682275cfcc9fd8b13bda99b407e96386b9bdc
Ramon Bauerfeind <ramonbauerfeind@web.de>
2019-11-01 19:35:06 +0100

    changed name "TextBox" to "TextBlock"

--- Sudoku_WPF/MainWindow.xaml.cs
+++ Sudoku_WPF/MainWindow.xaml.cs
@@ -32,13 +32,13 @@ namespace Sudoku
             int solved = 0;
             Sudoku.Solve(0, 0, 0, input, ref counter, ref solved, 0);
 
-            List<TextBlock> textBox = new List<TextBlock>();
+            List<TextBlock> TextBlock = new List<TextBlock>();
             for(int i = 0; i < 81; i++)
             {
-                textBox.Add(tb_1); textBox.Add(tb_2); textBox.Add(tb_3); textBox.Add(tb_4); textBox.Add(tb_5); textBox.Add(tb_6); textBox.Add(tb_7); textBox.Add(tb_8); textBox.Add(tb_9);
-                textBox.Add(tb_10); textBox.Add(tb_11); textBox.Add(tb_12); textBox.Add(tb_13); textBox.Add(tb_14); textBox.Add(tb_15); textBox.Add(tb_16); textBox.Add(tb_17); textBox.Add(tb_18);
-                textBox.Add(tb_19); textBox.Add(tb_20); textBox.Add(tb_21); textBox.Add(tb_22); textBox.Add(tb_23); textBox.Add(tb_24); textBox.Add(tb_25); textBox.Add(tb_26); textBox.Add(tb_27);
-                textBox.Add(tb_28); textBox.Add(tb_29); textBox.Add(tb_30); textBox.Add(tb_31); textBox.Add(tb_32); textBox.Add(tb_33); textBox.Add(tb_34); textBox.Add(tb_35); textBox.Add(tb_36);
+                TextBlock.Add(tb_1); TextBlock.Add(tb_2); TextBlock.Add(tb_3); TextBlock.Add(tb_4); TextBlock.Add(tb_5); TextBlock.Add(tb_6); TextBlock.Add(tb_7); TextBlock.Add(tb_8); TextBlock.Add(tb_9);
+                TextBlock.Add(tb_10); TextBlock.Add(tb_11); TextBlock.Add(tb_12); TextBlock.Add(tb_13); TextBlock.Add(tb_14); TextBlock.Add(tb_15); TextBlock.Add(tb_16); TextBlock.Add(tb_17); TextBlock.Add(tb_18);
+                TextBlock.Add(tb_19); TextBlock.Add(tb_20); TextBlock.Add(tb_21); TextBlock.Add(tb_22); TextBlock.Add(tb_23); TextBlock.Add(tb_24); TextBlock.Add(tb_25); TextBlock.Add(tb_26); TextBlock.Add(tb_27);
+                TextBlock.Add(tb_28); TextBlock.Add(tb_29); TextBlock.Add(tb_30); TextBlock.Add(tb_31); TextBlock.Add(tb_32); TextBlock.Add(tb_33); TextBlock.Add(tb_34); TextBlock.Add(tb_35); TextBlock.Add(tb_36);
             }
 
             int k = 0;
@@ -46,7 +46,7 @@ namespace Sudoku
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    textBox[k].Text = Convert.ToString(input[i, j]);
+                    TextBlock[k].Text = Convert.ToString(input[i, j]);
                     k++;
                 }
             }
