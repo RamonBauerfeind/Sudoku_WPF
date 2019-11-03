0f07f8504b19c02a6b3d6f710dc494641ab75840
Ramon Bauerfeind <ramonbauerfeind@web.de>
2019-11-03 18:40:40 +0100

    changed from TextBlock to Textfield, made game playable

--- Sudoku_WPF/MainWindow.xaml.cs
+++ Sudoku_WPF/MainWindow.xaml.cs
@@ -21,18 +21,18 @@ namespace Sudoku
         {
             InitializeComponent();
 
-            List<TextBlock> TextBlock = new List<TextBlock>();
+            List<TextBox> TextBox = new List<TextBox>();
             for (int i = 0; i < 81; i++)
             {
-                TextBlock.Add(tb_1); TextBlock.Add(tb_2); TextBlock.Add(tb_3); TextBlock.Add(tb_4); TextBlock.Add(tb_5); TextBlock.Add(tb_6); TextBlock.Add(tb_7); TextBlock.Add(tb_8); TextBlock.Add(tb_9);
-                TextBlock.Add(tb_10); TextBlock.Add(tb_11); TextBlock.Add(tb_12); TextBlock.Add(tb_13); TextBlock.Add(tb_14); TextBlock.Add(tb_15); TextBlock.Add(tb_16); TextBlock.Add(tb_17); TextBlock.Add(tb_18);
-                TextBlock.Add(tb_19); TextBlock.Add(tb_20); TextBlock.Add(tb_21); TextBlock.Add(tb_22); TextBlock.Add(tb_23); TextBlock.Add(tb_24); TextBlock.Add(tb_25); TextBlock.Add(tb_26); TextBlock.Add(tb_27);
-                TextBlock.Add(tb_28); TextBlock.Add(tb_29); TextBlock.Add(tb_30); TextBlock.Add(tb_31); TextBlock.Add(tb_32); TextBlock.Add(tb_33); TextBlock.Add(tb_34); TextBlock.Add(tb_35); TextBlock.Add(tb_36);
-                TextBlock.Add(tb_37); TextBlock.Add(tb_38); TextBlock.Add(tb_39); TextBlock.Add(tb_40); TextBlock.Add(tb_41); TextBlock.Add(tb_42); TextBlock.Add(tb_43); TextBlock.Add(tb_44); TextBlock.Add(tb_45);
-                TextBlock.Add(tb_46); TextBlock.Add(tb_47); TextBlock.Add(tb_48); TextBlock.Add(tb_49); TextBlock.Add(tb_50); TextBlock.Add(tb_51); TextBlock.Add(tb_52); TextBlock.Add(tb_53); TextBlock.Add(tb_54);
-                TextBlock.Add(tb_55); TextBlock.Add(tb_56); TextBlock.Add(tb_57); TextBlock.Add(tb_58); TextBlock.Add(tb_59); TextBlock.Add(tb_60); TextBlock.Add(tb_61); TextBlock.Add(tb_62); TextBlock.Add(tb_63);
-                TextBlock.Add(tb_64); TextBlock.Add(tb_65); TextBlock.Add(tb_66); TextBlock.Add(tb_67); TextBlock.Add(tb_68); TextBlock.Add(tb_69); TextBlock.Add(tb_70); TextBlock.Add(tb_71); TextBlock.Add(tb_72);
-                TextBlock.Add(tb_73); TextBlock.Add(tb_74); TextBlock.Add(tb_75); TextBlock.Add(tb_76); TextBlock.Add(tb_77); TextBlock.Add(tb_78); TextBlock.Add(tb_79); TextBlock.Add(tb_80); TextBlock.Add(tb_81);
+                TextBox.Add(tb_1); TextBox.Add(tb_2); TextBox.Add(tb_3); TextBox.Add(tb_4); TextBox.Add(tb_5); TextBox.Add(tb_6); TextBox.Add(tb_7); TextBox.Add(tb_8); TextBox.Add(tb_9);
+                TextBox.Add(tb_10); TextBox.Add(tb_11); TextBox.Add(tb_12); TextBox.Add(tb_13); TextBox.Add(tb_14); TextBox.Add(tb_15); TextBox.Add(tb_16); TextBox.Add(tb_17); TextBox.Add(tb_18);
+                TextBox.Add(tb_19); TextBox.Add(tb_20); TextBox.Add(tb_21); TextBox.Add(tb_22); TextBox.Add(tb_23); TextBox.Add(tb_24); TextBox.Add(tb_25); TextBox.Add(tb_26); TextBox.Add(tb_27);
+                TextBox.Add(tb_28); TextBox.Add(tb_29); TextBox.Add(tb_30); TextBox.Add(tb_31); TextBox.Add(tb_32); TextBox.Add(tb_33); TextBox.Add(tb_34); TextBox.Add(tb_35); TextBox.Add(tb_36);
+                TextBox.Add(tb_37); TextBox.Add(tb_38); TextBox.Add(tb_39); TextBox.Add(tb_40); TextBox.Add(tb_41); TextBox.Add(tb_42); TextBox.Add(tb_43); TextBox.Add(tb_44); TextBox.Add(tb_45);
+                TextBox.Add(tb_46); TextBox.Add(tb_47); TextBox.Add(tb_48); TextBox.Add(tb_49); TextBox.Add(tb_50); TextBox.Add(tb_51); TextBox.Add(tb_52); TextBox.Add(tb_53); TextBox.Add(tb_54);
+                TextBox.Add(tb_55); TextBox.Add(tb_56); TextBox.Add(tb_57); TextBox.Add(tb_58); TextBox.Add(tb_59); TextBox.Add(tb_60); TextBox.Add(tb_61); TextBox.Add(tb_62); TextBox.Add(tb_63);
+                TextBox.Add(tb_64); TextBox.Add(tb_65); TextBox.Add(tb_66); TextBox.Add(tb_67); TextBox.Add(tb_68); TextBox.Add(tb_69); TextBox.Add(tb_70); TextBox.Add(tb_71); TextBox.Add(tb_72);
+                TextBox.Add(tb_73); TextBox.Add(tb_74); TextBox.Add(tb_75); TextBox.Add(tb_76); TextBox.Add(tb_77); TextBox.Add(tb_78); TextBox.Add(tb_79); TextBox.Add(tb_80); TextBox.Add(tb_81);
             }
 
             int k = 0;
@@ -40,7 +40,7 @@ namespace Sudoku
             {
                 for (int j = 0; j < 9; j++)
                 {
-                    TextBlock[k].Text = Convert.ToString("");
+                    TextBox[k].Text = Convert.ToString("");
                     k++;
                 }
             }
@@ -49,23 +49,23 @@ namespace Sudoku
         public void NewGame()
         {
             int[,] input;
-            input = Sudoku.Create(0); // Schwierigkeitsgrad hier festlegen
+            input = Sudoku.Create(50); // Schwierigkeitsgrad hier festlegen
             int counter = 0;
             int solved = 0;
             Sudoku.Solve(0, 0, 0, input, ref counter, ref solved, 0);
 
-            List<TextBlock> TextBlock = new List<TextBlock>();
+            List<TextBox> TextBox = new List<TextBox>();
             for (int i = 0; i < 81; i++)
             {
-                TextBlock.Add(tb_1); TextBlock.Add(tb_2); TextBlock.Add(tb_3); TextBlock.Add(tb_4); TextBlock.Add(tb_5); TextBlock.Add(tb_6); TextBlock.Add(tb_7); TextBlock.Add(tb_8); TextBlock.Add(tb_9);
-                TextBlock.Add(tb_10); TextBlock.Add(tb_11); TextBlock.Add(tb_12); TextBlock.Add(tb_13); TextBlock.Add(tb_14); TextBlock.Add(tb_15); TextBlock.Add(tb_16); TextBlock.Add(tb_17); TextBlock.Add(tb_18);
-                TextBlock.Add(tb_19); TextBlock.Add(tb_20); TextBlock.Add(tb_21); TextBlock.Add(tb_22); TextBlock.Add(tb_23); TextBlock.Add(tb_24); TextBlock.Add(tb_25); TextBlock.Add(tb_26); TextBlock.Add(tb_27);
-                TextBlock.Add(tb_28); TextBlock.Add(tb_29); TextBlock.Add(tb_30); TextBlock.Add(tb_31); TextBlock.Add(tb_32); TextBlock.Add(tb_33); TextBlock.Add(tb_34); TextBlock.Add(tb_35); TextBlock.Add(tb_36);
-                TextBlock.Add(tb_37); TextBlock.Add(tb_38); TextBlock.Add(tb_39); TextBlock.Add(tb_40); TextBlock.Add(tb_41); TextBlock.Add(tb_42); TextBlock.Add(tb_43); TextBlock.Add(tb_44); TextBlock.Add(tb_45);
-                TextBlock.Add(tb_46); TextBlock.Add(tb_47); TextBlock.Add(tb_48); TextBlock.Add(tb_49); TextBlock.Add(tb_50); TextBlock.Add(tb_51); TextBlock.Add(tb_52); TextBlock.Add(tb_53); TextBlock.Add(tb_54);
-                TextBlock.Add(tb_55); TextBlock.Add(tb_56); TextBlock.Add(tb_57); TextBlock.Add(tb_58); TextBlock.Add(tb_59); TextBlock.Add(tb_60); TextBlock.Add(tb_61); TextBlock.Add(tb_62); TextBlock.Add(tb_63);
-                TextBlock.Add(tb_64); TextBlock.Add(tb_65); TextBlock.Add(tb_66); TextBlock.Add(tb_67); TextBlock.Add(tb_68); TextBlock.Add(tb_69); TextBlock.Add(tb_70); TextBlock.Add(tb_71); TextBlock.Add(tb_72);
-                TextBlock.Add(tb_73); TextBlock.Add(tb_74); TextBlock.Add(tb_75); TextBlock.Add(tb_76); TextBlock.Add(tb_77); TextBlock.Add(tb_78); TextBlock.Add(tb_79); TextBlock.Add(tb_80); TextBlock.Add(tb_81);
+                TextBox.Add(tb_1); TextBox.Add(tb_2); TextBox.Add(tb_3); TextBox.Add(tb_4); TextBox.Add(tb_5); TextBox.Add(tb_6); TextBox.Add(tb_7); TextBox.Add(tb_8); TextBox.Add(tb_9);
+                TextBox.Add(tb_10); TextBox.Add(tb_11); TextBox.Add(tb_12); TextBox.Add(tb_13); TextBox.Add(tb_14); TextBox.Add(tb_15); TextBox.Add(tb_16); TextBox.Add(tb_17); TextBox.Add(tb_18);
+                TextBox.Add(tb_19); TextBox.Add(tb_20); TextBox.Add(tb_21); TextBox.Add(tb_22); TextBox.Add(tb_23); TextBox.Add(tb_24); TextBox.Add(tb_25); TextBox.Add(tb_26); TextBox.Add(tb_27);
+                TextBox.Add(tb_28); TextBox.Add(tb_29); TextBox.Add(tb_30); TextBox.Add(tb_31); TextBox.Add(tb_32); TextBox.Add(tb_33); TextBox.Add(tb_34); TextBox.Add(tb_35); TextBox.Add(tb_36);
+                TextBox.Add(tb_37); TextBox.Add(tb_38); TextBox.Add(tb_39); TextBox.Add(tb_40); TextBox.Add(tb_41); TextBox.Add(tb_42); TextBox.Add(tb_43); TextBox.Add(tb_44); TextBox.Add(tb_45);
+                TextBox.Add(tb_46); TextBox.Add(tb_47); TextBox.Add(tb_48); TextBox.Add(tb_49); TextBox.Add(tb_50); TextBox.Add(tb_51); TextBox.Add(tb_52); TextBox.Add(tb_53); TextBox.Add(tb_54);
+                TextBox.Add(tb_55); TextBox.Add(tb_56); TextBox.Add(tb_57); TextBox.Add(tb_58); TextBox.Add(tb_59); TextBox.Add(tb_60); TextBox.Add(tb_61); TextBox.Add(tb_62); TextBox.Add(tb_63);
+                TextBox.Add(tb_64); TextBox.Add(tb_65); TextBox.Add(tb_66); TextBox.Add(tb_67); TextBox.Add(tb_68); TextBox.Add(tb_69); TextBox.Add(tb_70); TextBox.Add(tb_71); TextBox.Add(tb_72);
+                TextBox.Add(tb_73); TextBox.Add(tb_74); TextBox.Add(tb_75); TextBox.Add(tb_76); TextBox.Add(tb_77); TextBox.Add(tb_78); TextBox.Add(tb_79); TextBox.Add(tb_80); TextBox.Add(tb_81);
             }
 
             int k = 0;
@@ -75,12 +75,12 @@ namespace Sudoku
                 {
                     if (input[i, j] == 0)
                     {
-                        TextBlock[k].Text = Convert.ToString("");
+                        TextBox[k].Text = Convert.ToString("");
                         k++;
                     }
                     else
                     {
-                        TextBlock[k].Text = Convert.ToString(input[i, j]);
+                        TextBox[k].Text = Convert.ToString(input[i, j]);
                         k++;
                     }
                 }
